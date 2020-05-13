52bb75178408c66ce3b2c88e0c5836c275d77ebc
GRMCosta <rafa_gian2011@hotmail.com>
2020-05-12 21:37:00 -0300

    first commit

---
+++ src/main/java/com/projeto/changebooktransactions/ChangebookTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.changebooktransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangebookTransactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChangebookTransactionsApplication.class, args);
+    }
+
+}
